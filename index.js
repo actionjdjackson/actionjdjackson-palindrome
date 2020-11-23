6e0eb22540b35c27ce46e84337d1d0e450245bfd
Jacob Jackson <jacobjackson1985@icloud.com>
2020-11-23 15:17:31 -0600

    Initialize repository

---
+++ index.js
@@ -0,0 +1,22 @@
+String.prototype.reverse = function reverse() {
+  return Array.from(this).reverse().join("");
+}
+
+//Defines a Phrase object
+function Phrase(content) {
+  this.content = content;
+
+  this.processedContent = function processedContent() {
+    return this.content.toLowerCase();
+  }
+
+  //Returns true for a palindrome, false otherwise
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+
+}
+
+String.prototype.palindrome = function palindrome() {
+  return new Phrase(this).palindrome();
+}
